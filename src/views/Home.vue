330038481adc03fda451a8c0b666b17316b583d5
Cali Castle <cali@calicastle.com>
2019-03-09 00:08:45 -0800

    Update home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
-    <p class="flex"></p>
+    <div class="flex flex-col w-full h-screen justify-center items-center">
+      <p id="game-title" class="text-red-light">
+        Knife 2 Meat U
+      </p>
+      <p class="text-grey text-5xl">
+        Coming soon...
+      </p>
+    </div>
   </div>
 </template>
 
@@ -9,3 +16,8 @@ export default {
   name: 'home'
 }
 </script>
+
+<style lang="stylus">
+#game-title
+  font-size 15vmin
+</style>
