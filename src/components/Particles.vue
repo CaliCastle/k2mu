bc46bc8f7b29562b442b6bbee54993cc7f0373e0
Cali Castle <cali@calicastle.com>
2019-03-10 19:45:08 -0700

    Add particle juice

---
+++ src/components/Particles.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="particles" class="fixed pin-t pin-l w-screen h-screen"></div>
+</template>
+
+<script>
+import particles from '../assets/particles.json'
+
+export default {
+  name: 'particles',
+  mounted() {
+    window.particlesJS('particles', particles)
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,16 +3,18 @@
     <ComingSoon v-if="!ready" />
     <main v-else></main>
     <KnifeCursor />
+    <Particles />
   </div>
 </template>
 
 <script>
 import ComingSoon from '../components/ComingSoon'
 import KnifeCursor from '../components/KnifeCursor'
+import Particles from '../components/Particles'
 
 export default {
   name: 'home',
-  components: { ComingSoon, KnifeCursor },
+  components: { Particles, ComingSoon, KnifeCursor },
   data() {
     return {
       ready: false
